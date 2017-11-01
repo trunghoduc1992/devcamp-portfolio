9bb8ddcca7833026a7a6feff3eb1ed39bcfbaba4
Ho Duc Trung <trunghoduc_1992@yahoo.com.vn>
2017-11-01 10:42:41 +0700

    Added portfolio items via resources generator

---
+++ db/migrate/20171101033750_create_portfolios.rb
@@ -0,0 +1,12 @@
+class CreatePortfolios < ActiveRecord::Migration[5.1]
+  def change
+    create_table :portfolios do |t|
+      t.string :title
+      t.text :subtitle
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
