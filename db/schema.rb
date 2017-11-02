985d56ba67452e08e8bbdff8f8c4637f71927d3b
Ho Duc Trung <trunghoduc_1992@yahoo.com.vn>
2017-11-02 17:18:37 +0700

    Implemented index page for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101033750) do
+ActiveRecord::Schema.define(version: 20171102101125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20171101033750) do
     t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
   create_table "skills", force: :cascade do |t|
